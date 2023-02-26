9e76daf859817f055acf212d1238856f22acaa2f
Thomas Thrien <thomas.thrien@tquadrat.org>
2023-02-26 16:15:54 +0100

    Some minor changes (2023-02-26T15:15:01.591574395Z)

--- src/main/java/org/tquadrat/foundation/config/ap/impl/PropertySpecImpl.java
+++ src/main/java/org/tquadrat/foundation/config/ap/impl/PropertySpecImpl.java
@@ -34,7 +34,7 @@ import static org.tquadrat.foundation.config.ap.impl.codebuilders.CodeGeneratorC
 import static org.tquadrat.foundation.config.ap.impl.codebuilders.CodeGeneratorContext.getSetterComposer;
 import static org.tquadrat.foundation.lang.CommonConstants.NUL;
 import static org.tquadrat.foundation.lang.Objects.isNull;
-import static org.tquadrat.foundation.lang.Objects.mapFromNull;
+import static org.tquadrat.foundation.lang.Objects.mapNonNull;
 import static org.tquadrat.foundation.lang.Objects.nonNull;
 import static org.tquadrat.foundation.lang.Objects.requireNonNullArgument;
 import static org.tquadrat.foundation.lang.Objects.requireNotEmptyArgument;
@@ -944,7 +944,7 @@ public final class PropertySpecImpl implements PropertySpec
     public void setINIConfiguration( final INIValue configuration )
     {
         final var group = requireNonNullArgument( configuration, "configuration" ).group();
-        final var key = mapFromNull( configuration.key(), this::getPropertyName );
+        final var key = mapNonNull( configuration.key(), k -> isEmptyOrBlank( k ) ? getPropertyName() : k );
         final var comment = configuration.comment();
         setINIConfiguration( group, key, comment );
     }   //  setINIConfiguration()
